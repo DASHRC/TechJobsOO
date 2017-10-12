0d8d47958e9ebd2f25a58e35de2e1f41245909b9
diegosanchez <diegosanchez@bearfootworks.com>
2017-10-12 11:13:27 -0400

    got the Job with the given ID and passed it into the view

--- src/TechJobs/Controllers/JobController.cs
+++ src/TechJobs/Controllers/JobController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TechJobs.Data;
+using TechJobs.Models;
 using TechJobs.ViewModels;
 
 namespace TechJobs.Controllers
@@ -7,7 +8,7 @@ namespace TechJobs.Controllers
     public class JobController : Controller
     {
 
-        // Our reference to the data store
+   
         private static JobData jobData;
 
         static JobController()
@@ -18,9 +19,9 @@ namespace TechJobs.Controllers
         // The detail display for a given Job at URLs like /Job?id=17
         public IActionResult Index(int id)
         {
-            // TODO #1 - get the Job with the given ID and pass it into the view
-
-            return View();
+            // TODO #1 CHECK - get the Job with the given ID and pass it into the view
+            Job requestedJob = jobData.Find(id); //stores Job object (peek definition of Find to see the method in the Job class
+            return View(requestedJob);
         }
 
         public IActionResult New()
@@ -29,12 +30,11 @@ namespace TechJobs.Controllers
             return View(newJobViewModel);
         }
 
-        [HttpPost]
-        public IActionResult New(NewJobViewModel newJobViewModel)
-        {
-            // TODO #6 - Validate the ViewModel and if valid, create a 
-            // new Job and add it to the JobData data store. Then
-            // redirect to the Job detail (Index) action/view for the new Job.
+       
+
+                jobData.Jobs.Add(newJob);
+                return Redirect("/Job?id=" + newJob.ID);
+            }
 
             return View(newJobViewModel);
         }
